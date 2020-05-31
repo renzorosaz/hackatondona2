c5656edd5b50034e60386c7c1cac8e9b0626dc93
renzo1440 <43652358+renzorosaz@users.noreply.github.com>
2020-05-30 19:24:24 -0500

    Add files via upload
    commit actualizado perfil

--- lib/src/pages/perfil_page.dart
+++ lib/src/pages/perfil_page.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class PerfilPage extends StatelessWidget {
-  const PerfilPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +12,17 @@ class PerfilPage extends StatelessWidget {
         title: Text('PERFIL'),
       ),
       body: Stack(
-        children: <Widget>[
-          _fondoApp(),
-
-          SingleChildScrollView(
-            child: Column(
+        children: <Widget>[ 
+             SingleChildScrollView(
+            child: Column(              
               children: <Widget>[
-                _infoPerfil(),
-                _infoCoins(context),
-                _botonDonar(context),
-                _infoMedallas(context) 
+                Column(
+                  children: <Widget>[
+                    _infoPerfil(context),
+                     _infoDetalle(context),
+                    _infoMedallas(context) 
+                  ],
+                ),
               ],
             ),
           )
@@ -34,232 +34,180 @@ class PerfilPage extends StatelessWidget {
     );
   }
 
-  Widget _fondoApp(){
+ 
 
-    final gradiente = Container(
-      width:double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: FractionalOffset(0.0, 0.6),
-          end:FractionalOffset(0.0, 1.0),
-          colors: [
-            Color.fromRGBO(255, 255, 255, 1.0),
-            Color.fromRGBO(250, 250, 250, 1.0),
-          ]
-          )
-      ),
-    );
-
-      final cajaCeleste=Transform.rotate(
-        angle: -pi/4.0,
-
-        child:Container(
-          height: 360.0,
-          width: 360.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(80.0),
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(91, 236, 236, 1.0),
-                Color.fromRGBO(159, 236, 236, 1.0),
-              ]
-            )
-          ),
-        )
-     );
-
-
-    return Stack(
-      children: <Widget>[
-        gradiente,
-        Positioned(
-          top:-100.0,
-          child: cajaCeleste
-          )
-      ],
-    );
-
-  }
-
-  Widget _infoPerfil(){
+  Widget _infoPerfil(BuildContext context){
   
     return SafeArea(
-        child: Container(          
-          height: 180.0,
-          padding: EdgeInsets.all(10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child:  Container(
+         padding: EdgeInsets.all(15.0),
+        margin: EdgeInsets.symmetric(horizontal: 15.0,vertical: 20.0),      
+      
+          child: Column(       
+        children: <Widget>[
+          Row(            
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,            
             children: <Widget>[
-
-              Center(
-                child: Card(
-                     child: Image.asset('assets/beba.jpg', height: 180.0, ),
-                    elevation: 50.0,
-                    shape: CircleBorder(),
-                    clipBehavior: Clip.antiAlias,
-                  ),
-               
+              Column(       
+                crossAxisAlignment: CrossAxisAlignment.start,         
+                children: <Widget>[
+                  Image.asset('assets/dona.jpg', height: 100.0),                  
+                ],
               ),
-
-               Text('Josefina Flutter',style: TextStyle(color:Colors.white,fontSize:25.0,fontWeight: FontWeight.bold),),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                          
+                    children: <Widget>[
+                    Text('Robiño Poliño',style: TextStyle(color:Colors.black,fontSize:15.0,fontWeight: FontWeight.bold),),   
+                    Divider(), 
+                   Text('200 Donacoins',style: TextStyle(color:Colors.lightBlue[300],fontSize:13.0,fontWeight: FontWeight.w600),),
+                   Text('Código: sehi3939 ',style: TextStyle(color:Colors.black,fontSize:13.0,fontWeight: FontWeight.w600),),
+                  
+                ],
+              ),
             ],
-          ),
+          )
+        ],
+      ),
         )
     );
+  
   }
 
   Widget _infoCoins(BuildContext context){
-  
+    
     return SafeArea(      
           child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[            
-           _crearBotonRedondeadoDona(context, Colors.pinkAccent[700],'DonaCoins'),
-         
-          ],
-        ),
+            
+            padding: EdgeInsets.all(20.0),
+           margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 20.0),
+           
+           child: Column(   
+                    mainAxisAlignment: MainAxisAlignment.end,           
+                    children: <Widget>[
+                    Text('Robiño Poliño',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.bold),),   
+                    Divider(),        
+                   Text('200 Donacoins',style: TextStyle(color:Colors.lightBlue[300],fontSize:20.0,fontWeight: FontWeight.w600),),
+                   Text('Código: sehi3939 ',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.w600),),
+                      
+                ],
+              ),            
+          ),
+    );
+  }
+
+  Widget _infoDetalle(BuildContext context){
+
+    return Container(
+      padding: EdgeInsets.all(15.0),
+      margin: EdgeInsets.symmetric(horizontal: 15.0,vertical: 20.0),
+      
+      
+      child: Column(       
+        children: <Widget>[
+          Row(
+            
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            
+            children: <Widget>[
+              Column(       
+                crossAxisAlignment: CrossAxisAlignment.start,         
+                children: <Widget>[
+                  Text('Puntos Ganados',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.w300),), 
+                  Text('Puntos Verificados',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.w300),),
+                  Text('Puntos Canjeados',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.w300),),
+                  Text('Mis Donacoins',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.w300),),
+                  Text('1 donacion= 20 puntos verificados',style: TextStyle(color:Colors.black,fontSize:10.0,fontWeight: FontWeight.w300),),
+                  
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,  
+                children: <Widget>[
+                  Text('100',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.w300),), 
+                  Text('70',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.w300),), 
+                  Text('20',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.w300),), 
+                  Text('3.5 ',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.w300),), 
+                  
+                ],
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
 
    Widget _infoMedallas(BuildContext context){
   
-   return SafeArea(      
+    
+    final size= MediaQuery.of(context).size;
+    
+    return SafeArea(      
           child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-           _crearBotonRedondeadoRecom(context, Colors.pinkAccent[700],'Medallas'),
-         
-          ],
-        ),
-      ),
+            
+              padding: EdgeInsets.all(15.0),
+              margin: EdgeInsets.symmetric(horizontal: 15.0,vertical: 20.0),
+            
+            child: Column(    
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                    Text('Progreso en Donaciones',style: TextStyle(color:Colors.black,fontSize:20.0,fontWeight: FontWeight.w300),),
+                        
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        
+                         _crearBotonRedondeadoMedallas(Colors.lightBlueAccent,Icons.flash_auto,'1 DONAS'),
+                         _crearBotonRedondeadoMedallas(Colors.lightBlueAccent[50],Icons.flash_auto,'5 DONAS'),
+                         _crearBotonRedondeadoMedallas(Colors.lightBlueAccent[100],Icons.flash_auto,' 10 DONAS'),
+                      ],                      
+                    ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                         _crearBotonRedondeadoMedallas(Colors.lightBlueAccent[150],Icons.flash_auto,'25 DONAS'),
+                         _crearBotonRedondeadoMedallas(Colors.lightBlueAccent[200],Icons.flash_auto,'50 DONAS'),
+                         _crearBotonRedondeadoMedallas(Colors.lightBlueAccent[250],Icons.flash_auto,'100 DONAS'),
+                      ],                      
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                         _crearBotonRedondeadoMedallas(Colors.lightBlueAccent[300],Icons.flash_auto,'200 DONAS'),
+                         _crearBotonRedondeadoMedallas(Colors.lightBlueAccent[400],Icons.flash_auto,'500 DONAS'),
+                         _crearBotonRedondeadoMedallas(Colors.lightBlueAccent[50],Icons.flash_auto,'1000 DONAS'),
+                      ],                      
+                    ),
+                                 
+                ],
+              ),
+          ),
     );
 
 
     
   }
 
- Widget _crearBotonRedondeadoRecom(BuildContext context,Color color, String texto){
-
-      final size= MediaQuery.of(context).size;
-    
-     return ClipRect(        
-         child: Container(
-           padding: EdgeInsets.only(left: 20.0),
-           height: size.height * 0.50,
-           width: size.width * 0.90,
-           decoration: BoxDecoration(
-             color: Color.fromRGBO(62, 66, 107, 0.7),
-             borderRadius: BorderRadius.circular(20.0)
-           ),  
-           child: Column(
-             
-             mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                 Text('Recompensas Obtenidas',style: TextStyle(color:Colors.white,fontSize:20.0,fontWeight: FontWeight.bold),),
-                 _medallas(),
-                 
-              ],
-           ),
-
-
-         ),
-         
-
-     /*   ), */
-     );
-
-   }
-  
-   Widget _medallas(){
-     
-     return Container(
-
-      padding: EdgeInsets.all(10.0),
-      child: Table(        
-         children: [           
-           TableRow(
-             children: [
-               _crearBotonRedondeadoMedallas(Colors.pinkAccent[700],Icons.person,'Buena persona'),
-               _crearBotonRedondeadoMedallas(Colors.redAccent,Icons.business,'Llegaras alto')
-
-             ]
-           ), 
-           TableRow(
-             children: [
-               _crearBotonRedondeadoMedallas(Colors.pinkAccent[700],Icons.person,'Buena persona'),
-               _crearBotonRedondeadoMedallas(Colors.redAccent,Icons.business,'Llegaras alto')
-
-             ]
-           ),   
-             TableRow(
-             children: [
-               _crearBotonRedondeadoMedallas(Colors.pinkAccent[700],Icons.person,'Buena persona'),
-               _crearBotonRedondeadoMedallas(Colors.redAccent,Icons.business,'Llegaras alto')
-
-             ]
-           ),           
-         ],
-       ),
-    );
-   }
-
-
-  Widget _crearBotonRedondeadoDona(BuildContext context,Color color, String texto){
-
-      final size= MediaQuery.of(context).size;
-    
-     return ClipRect(        
-         child: Container(
-           height: size.height * 0.20,
-           width: size.width * 0.9,
-           decoration: BoxDecoration(
-             color: Color.fromRGBO(62, 66, 107, 0.7),
-             borderRadius: BorderRadius.circular(20.0)
-           ),  
-           child: Column(
-             mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                 Text('DonaCoins',style: TextStyle(color:Colors.white,fontSize:30.0,fontWeight: FontWeight.bold),),         
-                 Icon(Icons.flare,size: 50.0,),       
-                 Text('200 DonaCoins',style: TextStyle(color:Colors.white,fontSize:30.0,fontWeight: FontWeight.bold),)
-                 
-              ],
-
-           ),
-           
-
-         ),
-         
-
-     /*   ), */
-     );
-
-   }
 
   Widget _crearBotonRedondeadoMedallas(Color color, IconData icono, String texto){
-
 
      return ClipRect(
         /* /* child: BackdropFilter( */
          /* filter: ImageFilter.blur( sigmaX:10.0, sigmaY:10.0), */ */
          child: Container(
            height: 120.0,
-           width: 100.0,
-           margin: EdgeInsets.only(right: 20.0),
+           width: 100.0,           
            child: Column(
              mainAxisAlignment: MainAxisAlignment.spaceAround,
              children: <Widget>[
-               SizedBox(height: 5.0),
+               SizedBox(height: 15.0),              
                CircleAvatar(
                  backgroundColor: color,
-                 radius: 25.0,
-                 child: Icon( icono, color: Colors.white, size: 40.0),
+                 radius: 20.0,
+                 child: Icon( icono, color: Colors.white, size: 20.0),
                ),
                Text(texto,style: TextStyle( color: color )),
                SizedBox(height: 5.0)
@@ -270,26 +218,6 @@ class PerfilPage extends StatelessWidget {
      );
 
    }
-
-    Widget _botonDonar(BuildContext context){
-
-       
-          return RaisedButton(
-             child: Container(
-            padding: EdgeInsets.symmetric(horizontal:80.0, vertical:15.0),
-            child: Text('Quiero donar'),
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5.0)
-           ),
-           elevation: 0.0,
-           color:Colors.purple,
-           textColor: Colors.white,
-           onPressed:(null),
-          );
-
-     
-    } 
 
   Widget _bottomNavigatorBar(BuildContext context){
 
